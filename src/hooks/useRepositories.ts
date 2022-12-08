d887d47cecaebdb32c6008a7025b2f629004261c
Edvins Antonovs <ummahusla@gmail.com>
2022-12-08 22:57:10 +0200

    refactor: refactored some spaghetti code

--- src/hooks/useRepositories.ts
+++ src/hooks/useRepositories.ts
@@ -1,5 +1,6 @@
 import { ApolloError } from '@apollo/client';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { REPOS_PER_PAGE } from '../utils/constants';
 import useRepositoryList from './useRepositoryList';
 
 interface RawRepo {
@@ -31,7 +32,17 @@ export interface Repository {
 
 export function useRepositories(): [
   Repository[],
-  { loading: boolean; error: ApolloError | undefined }
+  {
+    loading: boolean;
+    error: ApolloError | undefined;
+  },
+  (searchInput: string) => void,
+  {
+    currentPage: number;
+    numberOfPages: number;
+    setCurrentPage: (currentPage: number) => void;
+    recordsNumber: number;
+  }
 ] {
   const { loading, error, data } = useRepositoryList();
 
@@ -54,11 +65,66 @@ export function useRepositories(): [
     });
   }, [data]);
 
+  const [searchInput, setSearchInput] = useState('');
+  const searchInProgress = searchInput !== '';
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastRecord = currentPage * REPOS_PER_PAGE;
+  const indexOfFirstRecord = indexOfLastRecord - REPOS_PER_PAGE;
+  const [recordsNumber, setRecordsNumber] = useState(repos.length);
+
+  const currentRepos = useMemo(
+    () => repos.slice(indexOfFirstRecord, indexOfLastRecord),
+    [indexOfFirstRecord, indexOfLastRecord, repos]
+  );
+
+  // Filter the repositories based on the search input
+  const filteredRepos = useMemo(() => {
+    if (!searchInput) return currentRepos;
+
+    return currentRepos.filter((repo: Repository) => {
+      // If the repository doesn't have a description, filter by name only
+      if (!repo.description)
+        return repo.name.toLowerCase().includes(searchInput.toLowerCase());
+
+      // If the repository has a description, filter by name and description
+      return (
+        repo.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        repo.description.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    });
+  }, [searchInput, currentRepos]);
+
+  // Calculate the total number of pages to display in pagination component
+  const numberOfPages = useMemo(() => {
+    return Math.ceil(
+      searchInProgress
+        ? filteredRepos.length / REPOS_PER_PAGE
+        : repos.length / REPOS_PER_PAGE
+    );
+  }, [filteredRepos.length, repos.length, searchInProgress]);
+
+  useEffect(() => {
+    // Calculate the total number of results to display in pagination component
+    if (searchInProgress) {
+      setRecordsNumber(filteredRepos.length);
+    } else {
+      setRecordsNumber(repos.length);
+    }
+  }, [filteredRepos.length, repos.length, searchInProgress, setRecordsNumber]);
+
   return [
-    repos || [],
+    filteredRepos,
     {
       loading,
       error,
+    },
+    setSearchInput,
+    {
+      currentPage,
+      numberOfPages,
+      setCurrentPage,
+      recordsNumber,
     },
   ];
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const REPO_TABLE_HEADERS = ['Name', 'Description', 'Stars', 'Forks'];
+export const REPOS_PER_PAGE = 10;
